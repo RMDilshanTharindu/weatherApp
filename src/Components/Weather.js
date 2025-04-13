16ec6259c9597a1f2964871b2340fb1f5b5df7b7
Dilshan_Tharindu <dilshantharinducamp@gmail.com>
2025-04-13 08:16:00 +0530

    UI part

---
+++ src/Components/Weather.js
@@ -0,0 +1,43 @@
+import './Weather.css'
+import search_icon from '../Assets/search.png'
+import clear_icon from '../Assets/clear.png'
+import cloud_icon from '../Assets/cloud.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import humidity_icon from '../Assets/humidity.png'
+import rain_icon from '../Assets/rain.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className='search-bar'>
+            <input type='text' placeholder='Search'/>
+            <img src={search_icon} alt=''/>
+        </div>
+        
+        <img src={clear_icon} alt='' className='weather-icon'/>
+        <p className='temperture'>16 C</p>
+        <p className='location'>London</p>
+        <div className='weather-data'>
+            <div className='col'>
+                <img src={humidity_icon} alt=''/>
+                <div>
+                    <p>91 %</p>
+                    <spna>Humidity</spna>
+                </div>
+            </div>
+            <div className='col'>
+                <img src={wind_icon} alt=''/>
+                <div>
+                    <p>3.6 km/h</p>
+                    <spna>Wind Speed</spna>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Weather;
